6737bf83783428a55ccfe0de162ee77647a27a25
Shlomi <shlomizvi09@campus.technion.ac.il>
2021-01-02 12:22:31 +0000

    started testing Semaphore

---
+++ wet3_files/Code_Skeleton/Part1/Semaphore.cpp
@@ -0,0 +1,33 @@
+#include <Semaphore.hpp>
+
+Semaphore::Semaphore() : counter(0) {
+    pthread_mutex_init(&m_lock, NULL);
+    pthread_cond_init(&condition, NULL);
+}
+
+Semaphore::Semaphore(unsigned val) : counter(val) {
+    pthread_mutex_init(&m_lock, NULL);
+    pthread_cond_init(&condition, NULL);
+}
+
+Semaphore::~Semaphore() {
+    pthread_mutex_destroy(&m_lock);
+    pthread_cond_destroy(&condition);
+}
+
+void Semaphore::down() {
+    pthread_mutex_lock(&m_lock);
+    while (counter == 0) {
+        pthread_cond_wait(&condition, &m_lock);
+    }
+    counter--;
+
+    pthread_mutex_unlock(&m_lock);
+}
+
+void Semaphore::up() {
+    pthread_mutex_lock(&m_lock);
+    counter++;
+    pthread_cond_signal(&condition);
+    pthread_mutex_unlock(&m_lock);
+}
--- wet3_files/Code_Skeleton/Part1/Semaphore.hpp
+++ wet3_files/Code_Skeleton/Part1/Semaphore.hpp
@@ -2,18 +2,22 @@
 #define __SEMAPHORE_H
 #include "Headers.hpp"
 
-// Synchronization Warm up 
+// Synchronization Warm up
 class Semaphore {
-public:
-	Semaphore(); // Constructs a new semaphore with a counter of 0
-	Semaphore(unsigned val); // Constructs a new semaphore with a counter of val
+   public:
+    Semaphore();              // Constructs a new semaphore with a counter of 0
+    Semaphore(unsigned val);  // Constructs a new semaphore with a counter of val
 
+    ~Semaphore();
 
-	void up(); // Mark: 1 Thread has left the critical section
-	void down(); // Block untill counter >0, and mark - One thread has entered the critical section.
+    void up();    // Mark: 1 Thread has left the critical section
+    void down();  // Block untill counter >0, and mark - One thread has entered the critical section.
 
-private:
-	// TODO 
+   private:
+    // TODO
+    unsigned int counter;
+    pthread_mutex_t m_lock;
+    pthread_cond_t condition;
 };
 
 #endif
